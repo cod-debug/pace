8f18d1c2fdcf52e4d565a892627582c6d0c39d28
Roy G. Dueñas <rduenas@ingenuiti.ph>
2023-09-19 20:27:49 +0800

    added partial employee record

--- database/migrations/2023_09_19_063239_create_employee_record_table.php
+++ database/migrations/2023_09_19_063239_create_employee_record_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('employee_record', function (Blueprint $table) {
             $table->id();
+            $table->date('payment_date');
+            $table->foreignId('employee_id')->references('id')->on('employees');
+            $table->string('name');
+            $table->string('relationship');
+            $table->string('status')->default('active');
             $table->timestamps();
         });
     }
